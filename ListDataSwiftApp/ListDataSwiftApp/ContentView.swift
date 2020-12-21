4147375bd9172c31f2b0fdce8e4a19ae374d7222
selfstudymaster <webselfmaster@gmail.com>
2020-12-21 21:03:06 +0900

    This is the first commit

---
+++ ListDataSwiftApp/ListDataSwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListDataSwiftApp
+//
+//  Created by mac on 2020/12/21.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
